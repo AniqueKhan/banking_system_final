3e268dc300dee19ce067510c7d128908e7865cb5
AniqueKhanABC <anique.khan@alphabetacore.com>
2024-04-23 14:03:12 +0500

    modified allowed hosts

--- banking_system/banking_system/settings.py
+++ banking_system/banking_system/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = not ON_VPS
 
 if ON_VPS:
-    ALLOWED_HOSTS = ['184.94.215.214']
+    ALLOWED_HOSTS = ['184.94.215.214','bankingapplication.pynabyte.com','www.bankingapplication.pynabyte.com']
 else:
     ALLOWED_HOSTS = ['localhost']
 
